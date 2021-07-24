49554dfcecbe906228cdc23b3719c55cd79cd595
PacCol <pacome.colinet1@gmail.com>
2021-07-24 23:58:22 +0200

    update

--- js/dark-theme.js
+++ js/dark-theme.js
@@ -9,12 +9,12 @@ $(".dark-theme-toggle-switch input").change(function() {
         $(".dark-theme-toggle-switch input").prop("checked", true);
         $("body").hide();
         $("body").addClass("dark-theme");
-        $("body").fadeIn(300);
+        $("body").fadeIn(500);
     } else {
         localStorage.setItem("theme", "light-theme");
         $(".dark-theme-toggle-switch input").prop("checked", false);
         $("body").hide();
         $("body").removeClass("dark-theme");
-        $("body").fadeIn(300);
+        $("body").fadeIn(500);
     }
 });
--- js/settings-item.js
+++ js/settings-item.js
@@ -1,10 +1,14 @@
-$(".settings-item p").click(function() {
+$(".settings-item").click(function() {
 
-    var toggleSwitch = $(this).closest(".settings-item").find(".toggle-switch input");
+    var toggleSwitch = $(this).find(".toggle-switch input");
 
     if (toggleSwitch.prop("checked")) {
         toggleSwitch.prop("checked", false).change();
     } else {
         toggleSwitch.prop("checked", true).change();
     }
+});
+
+$(".toggle-switch").click(function(e) {
+    e.stopPropagation();
 });
