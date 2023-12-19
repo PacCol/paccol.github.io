7dfafbc1f173a53cb401c6224ebf0ace933a5957
PacCol <pacome.colinet1@gmail.com>
2023-12-19 18:07:17 +0100

    update

--- js/accordion.js
+++ js/accordion.js
@@ -1,14 +1,16 @@
-var acc = document.getElementsByClassName("accordion-tab");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
+function refreshAccordions() {
+    var accordions = document.getElementsByClassName("accordion-tab");
+    for (let i = 0; i < accordions.length; i++) {
+        accordions[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
 }
+
+refreshAccordions();
